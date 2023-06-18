468c4c84ff1ed383839090c5f83b74606c26bd1f
mechaneyes <ray@mechaneyes.com>
2023-06-17 20:02:16 -0700

    pitchfork v langchain retrieval augmentation
    + implemented for pitchfork reviews found on kaggle
    + built on top of the langchain handbook notebooks

---
+++ README.md
@@ -0,0 +1,25 @@
+# Third Eyes
+Third Bridge Creative vs Mechaneyes
+
+Augmenting the Third Bridge corpus with artificial intelligence
+
+
+<br /><br />
+
+# RESTful API
+
+### How to connect Flask to React
+https://dev.to/nagatodev/how-to-connect-flask-to-reactjs-1k8i
+
+<br />
+
+### Developing RESTful APIs with Python and Flask
+https://auth0.com/blog/developing-restful-apis-with-python-and-flask/  
+
+https://auth0.com/blog/developing-restful-apis-with-python-and-flask/#-span-id--python-classes----span--Mapping-Models-with-Python-Classes  
+
+<br />
+
+### How to Get Python and JavaScript to Communicate Using JSON
+https://www.makeuseof.com/tag/python-javascript-communicate-json/
+
---
+++ dependencies-notes.sh
@@ -0,0 +1,19 @@
+pip install langchain==0.0.162 openai==0.27.0 tiktoken==0.4.0 'pinecone-client[grpc]==2.2.1' datasets==2.12.0 apache-beam==2.47.0 mwparserfromhell==0.6.4 protobuf==3.19.3 --verbose
+
+pip install langchain==0.0.162 openai==0.27.0 tiktoken==0.4.0 'pinecone-client[grpc]>=2.1.0' datasets==2.12.0 apache-beam==2.47.0 mwparserfromhell==0.6.4 'protobuf<4.23.0,>=3.20.1' --verbose
+
+pip install langchain==0.0.162 openai==0.27.0 tiktoken==0.4.0 'pinecone-client[grpc]==3.0.0rc2' datasets==2.12.0 apache-beam==2.47.0 mwparserfromhell==0.6.4 'protobuf<4.23.0,>=3.20.1' --verbose
+
+pip install langchain==0.0.162 --verbose
+pip install openai==0.27.0 --verbose
+pip install tiktoken==0.4.0 --verbose
+pip install 'pinecone-client[grpc]==3.0.0rc2' --verbose
+pip install datasets==2.12.0 --verbose
+pip install apache-beam==2.45.0 --verbose
+pip install mwparserfromhell==0.6.4 --verbose
+pip install 'protobuf==3.19.3' --verbose
+
+pip install langchain==0.0.162 openai==0.27.0 tiktoken==0.4.0 'pinecone-client[grpc]==3.0.0rc2' datasets==2.12.0 apache-beam==2.45.0 mwparserfromhell==0.6.4 'protobuf==3.19.3' --verbose
+
+
+%pip install langchain==0.0.162 openai==0.27.0 tiktoken==0.4.0 'pinecone-client[grpc]==2.0.1' datasets==2.12.0 apache-beam==2.45.0 mwparserfromhell==0.6.4 'protobuf==3.13.0' --verbose
